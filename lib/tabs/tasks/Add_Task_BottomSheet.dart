78ed79634834d1060dc89ef203d86e3762240226
HatemReda7 <hreda2567@gmail.com>
2023-10-18 19:04:36 +0300

    added validator to the textFormField

--- lib/tabs/tasks/Add_Task_BottomSheet.dart
+++ lib/tabs/tasks/Add_Task_BottomSheet.dart
@@ -22,6 +22,7 @@ class _AddTaskBottomSheetState extends State<AddTaskBottomSheet> {
   var descriptionController = TextEditingController();
 
   var selectedDate = DateTime.now();
+  var formKey=GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -30,98 +31,115 @@ class _AddTaskBottomSheetState extends State<AddTaskBottomSheet> {
       color: Theme.of(context).cardColor,
       child: Padding(
         padding: EdgeInsets.symmetric(vertical: 18.0.h, horizontal: 18.w),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              AppLocalizations.of(context)!.addNewTask,
-              textAlign: TextAlign.center,
-              style: GoogleFonts.poppins(
-                  color: Theme.of(context).indicatorColor,
-                  fontWeight: FontWeight.w700,
-                  fontSize: 18.sp),
-            ),
-            SizedBox(height: 20.h,),
-            TextFormField(
-              style: GoogleFonts.inter(
-                  color: pro.themeMode==ThemeMode.light? Colors.black : Colors.white,
+        child: Form(
+          key: formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                AppLocalizations.of(context)!.addNewTask,
+                textAlign: TextAlign.center,
+                style: GoogleFonts.poppins(
+                    color: Theme.of(context).indicatorColor,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 18.sp),
+              ),
+              SizedBox(height: 20.h,),
+              TextFormField(
+                validator: (value) {
+                  if(value== null || value.isEmpty){
+                    return "Please enter Task Title";
+                  }
+                  return null;
+                },
+                style: GoogleFonts.inter(
+                    color: pro.themeMode==ThemeMode.light? Colors.black : Colors.white,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16.sp),
+                controller: titleController,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: const BorderSide(color: primary),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: const BorderSide(color: primary),
+                  ),
+                  hintText: AppLocalizations.of(context)!.enterTaskTitle,
+                  hintStyle: GoogleFonts.inter(
+                      color: const Color(0xffA9A9A9).withOpacity(.61),
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20.sp),
+                ),
+              ),
+              SizedBox(height: 20.h,),
+              TextFormField(
+                validator: (value) {
+                  if(value== null || value.isEmpty){
+                    return "Please enter Task Description";
+                  }
+                  return null;
+                },
+                style: GoogleFonts.inter(
+              color: pro.themeMode==ThemeMode.light? Colors.black : Colors.white,
                   fontWeight: FontWeight.w400,
                   fontSize: 16.sp),
-              controller: titleController,
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                  borderSide: const BorderSide(color: primary),
+                controller: descriptionController,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: const BorderSide(color: primary),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: const BorderSide(color: primary),
+                  ),
+                  hintText: AppLocalizations.of(context)!.enterTaskDescription,
+                  hintStyle: GoogleFonts.inter(
+                      color: const Color(0xffA9A9A9).withOpacity(.61),
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20.sp),
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                  borderSide: const BorderSide(color: primary),
-                ),
-                hintText: AppLocalizations.of(context)!.enterTaskTitle,
-                hintStyle: GoogleFonts.inter(
-                    color: const Color(0xffA9A9A9).withOpacity(.61),
-                    fontWeight: FontWeight.w400,
-                    fontSize: 20.sp),
               ),
-            ),
-            SizedBox(height: 20.h,),
-            TextFormField(
-              style: GoogleFonts.inter(
-            color: pro.themeMode==ThemeMode.light? Colors.black : Colors.white,
-                fontWeight: FontWeight.w400,
-                fontSize: 16.sp),
-              controller: descriptionController,
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                  borderSide: const BorderSide(color: primary),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                  borderSide: const BorderSide(color: primary),
-                ),
-                hintText: AppLocalizations.of(context)!.enterTaskDescription,
-                hintStyle: GoogleFonts.inter(
-                    color: const Color(0xffA9A9A9).withOpacity(.61),
-                    fontWeight: FontWeight.w400,
-                    fontSize: 20.sp),
-              ),
-            ),
-            SizedBox(height: 20.h,),
-            Text(
-              AppLocalizations.of(context)!.selectTime,
-              style: GoogleFonts.inter(
-                  color: Theme.of(context).indicatorColor,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 20.sp),
-            ),
-            SizedBox(height: 10.h,),
-            InkWell(
-              onTap: () {
-                selectDate();
-              },
-              child: Text(
-                selectedDate.toString().substring(0, 10),
-                textAlign: TextAlign.center,
+              SizedBox(height: 20.h,),
+              Text(
+                AppLocalizations.of(context)!.selectTime,
                 style: GoogleFonts.inter(
-                    color: Theme.of(context).indicatorColor.withOpacity(.61),
+                    color: Theme.of(context).indicatorColor,
                     fontWeight: FontWeight.w400,
                     fontSize: 20.sp),
               ),
-            ),
-            SizedBox(height: 20.h,),
-            ElevatedButton(
-                onPressed: () {
-                  TaskModel taskModel= TaskModel(title: titleController.text, description: descriptionController.text, date: selectedDate.millisecondsSinceEpoch);
-                  FirebaseFunctions.addTask(taskModel);
-                  Navigator.pop(context, (route) => false);
+              SizedBox(height: 10.h,),
+              InkWell(
+                onTap: () {
+                  selectDate();
                 },
-                style: ButtonStyle(
-                    shape: MaterialStatePropertyAll(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)))),
-                child: Text(AppLocalizations.of(context)!.addTask,style: TextStyle(fontSize: 16.sp),))
-          ],
+                child: Text(
+                  selectedDate.toString().substring(0, 10),
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.inter(
+                      color: Theme.of(context).indicatorColor.withOpacity(.61),
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20.sp),
+                ),
+              ),
+              SizedBox(height: 20.h,),
+              ElevatedButton(
+                  onPressed: () {
+                    if(formKey.currentState!.validate()){
+                      TaskModel taskModel= TaskModel(title: titleController.text, description: descriptionController.text, date: selectedDate.millisecondsSinceEpoch);
+                      FirebaseFunctions.addTask(taskModel);
+                      Navigator.pop(context, (route) => false);
+                    }
+                  },
+                  style: ButtonStyle(
+                      shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)))),
+                  child: Text(AppLocalizations.of(context)!.addTask,style: TextStyle(fontSize: 16.sp),))
+            ],
+          ),
         ),
       ),
     );
