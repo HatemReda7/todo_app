b4c629c72afb6b3ea43c6e67647a4502c9219d4b
HatemReda7 <hreda2567@gmail.com>
2023-10-20 02:48:37 +0300

    edited main

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:islami_app/Screens/SplashScreen.dart';
-import 'package:islami_app/Screens/homescreen.dart';
-import 'package:islami_app/Shared/styles/myThemeData.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:islami_app/preference_helper.dart';
-import 'package:islami_app/providers/my_provider.dart';
-import 'package:islami_app/tabs/settings/Settings_Tab.dart';
-import 'package:islami_app/tabs/tasks/TaskEditTab.dart';
-import 'package:islami_app/tabs/tasks/To_Do_List_Tab.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'Screens/SplashScreen.dart';
+import 'Screens/homescreen.dart';
+import 'Shared/styles/myThemeData.dart';
 import 'firebase_options.dart';
+import 'preference_helper.dart';
+import 'providers/my_provider.dart';
+import 'tabs/settings/Settings_Tab.dart';
+import 'tabs/tasks/TaskEditTab.dart';
+import 'tabs/tasks/To_Do_List_Tab.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
